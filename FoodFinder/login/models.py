59de2fa017d2f16a420c619e13b188c4922675e9
RSeeth947 <rupesh.seeth@gmail.com>
2024-09-17 14:48:40 -0400

    no changes, needed to do this to checkout branch

--- FoodFinder/login/models.py
+++ FoodFinder/login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
 
-# Create your models here.
+
+class User(models.Model):
+    pass
--- FoodFinder/login/views.py
+++ FoodFinder/login/views.py
@@ -3,6 +3,8 @@ from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def login(request):
     template = loader.get_template('login.html')
     context = {}
