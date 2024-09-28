988ff4e4f8e337e9b7bc0a0b57dc73e95f991e29
msatish6 <msatish6@gatech.edu>
2024-09-28 15:48:05 -0400

    updated profile page, users can update fav cusine

--- FoodFinder/profilePage/views.py
+++ FoodFinder/profilePage/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from authentication.models import Profile  # Import the Profile model
 
 @login_required
 def profile_page(request):
+    username = request.user.username
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    
+    # Fetch the user's profile
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        # Get the favorite cuisine from the form
+        favorite_cusine = request.POST.get('favorite_cusine')
+        # Update the user's favorite cuisine
+        profile.favorite_cusine = favorite_cusine
+        profile.save()
+        return redirect('profile')  # Redirect to the same page to see changes
 
     x = ["hello", "test"]
-    context = {"x": x}
-
-
+    context = {
+        "greeting": f"hello {username}",
+        "first_name": first_name,
+        "last_name": last_name,
+        "favorite_cusine": profile.favorite_cusine,  # Pass favorite_cusine to the template
+        "x": x,
+    }
 
     return render(request, 'profile.html', context)
