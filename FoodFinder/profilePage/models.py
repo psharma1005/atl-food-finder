a693cb1133220b7b3ae83a67310c03a104c12ae0
msatish6 <msatish6@gatech.edu>
2024-09-28 16:36:29 -0400

    setup view favorites in db

--- FoodFinder/profilePage/models.py
+++ FoodFinder/profilePage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class FavoriteRestaurant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to User
+    name = models.CharField(max_length=255)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    address = models.CharField(max_length=255)
+    distance = models.DecimalField(max_digits=5, decimal_places=1)  # Distance in km
+
+    def __str__(self):
+        return f"{self.name} - {self.user.username}"
