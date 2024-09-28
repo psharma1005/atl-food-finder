f45cfe6f74be02759bf219e868908c3f80cc2246
RSeeth947 <rupesh.seeth@gmail.com>
2024-09-28 16:09:23 -0400

    fixed git conflicts

--- FoodFinder/FoodFinder/urls.py
+++ FoodFinder/FoodFinder/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin  # Django admin module
 from django.urls import path       # URL routing
 from authentication.views import *  # Import views from the authentication app
 from profilePage.views import *
+from restaurant_search.views import *
 from django.conf import settings   # Application settings
 # Static files serving
 
@@ -12,5 +13,6 @@ urlpatterns = [
     path('accounts/login/', login_page, name='login_page'),
     path('register/', register_page, name='register_page'),
     path('reset-password/', reset_password_page, name='reset_password_page'),
-    path('profile/', profile_page, name='profile')
+    path('profile/', profile_page, name='profile'),
+    path('search/', restaurant_search_view, name='search')
 ]
--- FoodFinder/profilePage/views.py
+++ FoodFinder/profilePage/views.py
@@ -8,24 +8,20 @@ def profile_page(request):
     first_name = request.user.first_name
     last_name = request.user.last_name
     
-    # Fetch the user's profile
     profile = Profile.objects.get(user=request.user)
 
     if request.method == 'POST':
-        # Get the favorite cuisine from the form
         favorite_cusine = request.POST.get('favorite_cusine')
-        # Update the user's favorite cuisine
         profile.favorite_cusine = favorite_cusine
         profile.save()
-        return redirect('profile')  # Redirect to the same page to see changes
+        return redirect('profile')  
 
-    x = ["hello", "test"]
     context = {
         "greeting": f"hello {username}",
         "first_name": first_name,
         "last_name": last_name,
         "favorite_cusine": profile.favorite_cusine,  # Pass favorite_cusine to the template
-        "x": x,
+        
     }
 
     return render(request, 'profile.html', context)
