f6b22791ba2bad3c9b7e52a53eb28637c44c01f1
uedaritsu534 <uedaritsu534@gmail.com>
2024-09-29 18:44:28 -0400

    edited search

--- FoodFinder/authentication/views.py
+++ FoodFinder/authentication/views.py
@@ -28,7 +28,7 @@ def login_page(request):
             return redirect('/login/')
         else:
             login(request, user)
-            return redirect('/home/')
+            return redirect('/search/')
 
     return render(request, 'login.html')
 
