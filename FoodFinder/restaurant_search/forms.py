e90611d67d8aba2437930bc8bdf093d2d8ef9483
uedaritsu534 <uedaritsu534@gmail.com>
2024-09-30 22:30:20 -0400

    added buttons

--- FoodFinder/restaurant_search/forms.py
+++ FoodFinder/restaurant_search/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class RestaurantSearchForm(forms.Form):
-    cuisine = forms.CharField(label='Cuisine Type', max_length=100)
+    cuisine = forms.CharField(label='Restaurant Name', max_length=100)
     min_rating = forms.FloatField(label='Minimum Rating (1-5)', min_value=1.0, max_value=5.0)
     max_distance = forms.FloatField(label='Maximum Distance (in km)', min_value=0.1)
